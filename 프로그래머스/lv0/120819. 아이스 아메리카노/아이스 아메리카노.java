0fc793c5a7f015c5e1e0bb32229d220b82c79100
pearyoon <106965001+pearyoon@users.noreply.github.com>
2023-04-14 15:11:28 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] solution(int money) {
         
-        int[] answer = {money/5500, money - 5500 * (money/5500)};
+        int[] answer = {money/5500, money%5500};
         
         return answer;
     }
