a24fe801e63c71d70320f91eb27db1471bb27149
pearyoon <106965001+pearyoon@users.noreply.github.com>
2023-04-16 21:44:07 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = {};
+        
+        if(n%2 == 0){
+            answer = new int[n/2];
+        } else{
+            answer = new int[n/2 + 1];
+        }
+        
+        int idx = 0;
+        for(int i = 1; i <= n; i++){
+            if(i%2 != 0){
+                answer[idx] = i;
+                idx++;
+            }
+        }
+        
+        return answer;
+    }
+}
